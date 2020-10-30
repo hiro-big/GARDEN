c198532ad14dc4e80062df9cfc12aaa5d3ca0b9b
西川大斗 <nishikawahiroto@nh-mac.local>
2020-10-30 16:21:01 +0900

    コンパイルをし、クラスファイルの作成

--- Trunstile.java
+++ Trunstile.java
@@ -1,11 +1,11 @@
 import java.lang.Thread;
 
-public class Trunstile extend Thread{
+public class Trunstile extends Thread{
   Counter counter = new Counter();
 
   public void run(){
     for(int i=1;i<100;i++){
-      system.out.println("来場"+(i));
+      System.out.println("来場"+(i));
       counter.writeValue(counter.readValue());
     }
   }
